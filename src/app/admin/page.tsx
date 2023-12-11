1c8daaeb9db6efe0699af115faaded7896d44af0
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-12-11 16:22:12 +0900

    got basic question flow going with admin

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,263 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { Choice, Player, Problem, Session, sessionId, supabase } from '../page'
+
+enum AdminScreens {
+  lobby,
+  quiz,
+  results,
+}
+
+export default function Home() {
+  const [currentScreen, setCurrentScreen] = useState<AdminScreens>(
+    AdminScreens.lobby
+  )
+
+  const [players, setPlayers] = useState<Player[]>([])
+
+  const [problems, setProblems] = useState<Problem[]>()
+
+  const setPlayerListener = async () => {
+    supabase
+      .channel('session')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'players',
+        },
+        (payload) => {
+          setPlayers((currentPlayers) => {
+            return [...currentPlayers, payload.new as Player]
+          })
+        }
+      )
+      .subscribe()
+
+    const { data, error } = await supabase.from('players').select()
+    const some = [
+      ...data!,
+      ...data!,
+      ...data!,
+      ...data!,
+      ...data!,
+      ...data!,
+      ...data!,
+      ...data!,
+      ...data!,
+    ]
+    setPlayers(some as Player[])
+  }
+
+  useEffect(() => {
+    setPlayerListener()
+    getProblems()
+    setSessionListner()
+  }, [])
+
+  const getProblems = async () => {
+    const { data, error } = await supabase
+      .from('problems')
+      .select(`*, choices(*)`)
+      .order('order', { ascending: true })
+    if (error) {
+      getProblems()
+      return
+    }
+    setProblems(data)
+
+    const choiceCount = data.map((rows: Problem) => rows.choices.length)
+
+    const correctCount = data.map(
+      (rows) =>
+        rows.choices.filter((choice: Choice) => choice.is_correct).length
+    )
+  }
+
+  const [currentProblemSequence, setCurrentProblemSequence] = useState(0)
+
+  const setSessionListner = () => {
+    supabase
+      .channel('session')
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'sessions',
+          filter: `id=eq.${sessionId}`,
+        },
+        (payload) => {
+          // start the quiz session
+          const session = payload.new as Session
+          setCurrentProblemSequence(session.current_problem_sequence)
+          if (session.is_done) {
+            setCurrentScreen(AdminScreens.results)
+          } else {
+            setCurrentScreen(AdminScreens.quiz)
+          }
+        }
+      )
+      .subscribe()
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <div className="m-auto p-8 bg-black  text-white">
+        {currentScreen == AdminScreens.lobby && (
+          <Lobby players={players}></Lobby>
+        )}
+        {currentScreen == AdminScreens.quiz && (
+          <Quiz
+            problem={problems![currentProblemSequence]}
+            problemCount={problems!.length}
+          ></Quiz>
+        )}
+      </div>
+    </main>
+  )
+}
+
+function Quiz({
+  problem,
+  problemCount,
+}: {
+  problem: Problem
+  problemCount: number
+}) {
+  const [hasShownAnswer, setHasShownAnswer] = useState(false)
+
+  const [hasShownChoices, setHasShownChoices] = useState(false)
+
+  const getNextQuestion = async () => {
+    var updateData
+    if (problemCount == problem.order + 1) {
+      updateData = { is_done: true }
+    } else {
+      updateData = { current_problem_sequence: problem.order + 1 }
+    }
+
+    const { data, error } = await supabase
+      .from('sessions')
+      .update(updateData)
+      .eq('id', sessionId)
+    if (error) {
+      return alert(error.message)
+    }
+  }
+
+  useEffect(() => {
+    setHasShownAnswer(false)
+    setHasShownChoices(false)
+
+    setTimeout(() => {
+      setHasShownChoices(true)
+    }, 5000)
+  }, [problem.id])
+
+  return (
+    <div>
+      <div className="absolute left-4 top-4">
+        {problem.order + 1}/{problemCount}
+      </div>
+
+      <h1 className="pb-4 text-xl">{problem.body}</h1>
+      {hasShownChoices && (
+        <div className="flex justify-between flex-wrap">
+          {problem.choices.map((choice) => (
+            <div key={choice.id} className="w-1/2 p-1">
+              <div
+                className={`p-2 w-full text-center 
+              ${
+                hasShownAnswer
+                  ? choice.is_correct
+                    ? 'bg-green-500'
+                    : 'bg-gray-400'
+                  : 'bg-green-500'
+              }`}
+              >
+                {choice.body}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      {!hasShownChoices && (
+        <div className="text-center">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      )}
+      <div className="flex justify-between pt-4">
+        <div></div>
+        {!hasShownAnswer && (
+          <button
+            className="p-2 bg-white text-black"
+            onClick={() => setHasShownAnswer(true)}
+          >
+            答えを見る
+          </button>
+        )}
+        {hasShownAnswer && (
+          <button className="p-2 bg-white text-black" onClick={getNextQuestion}>
+            次の問題へ
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function Lobby({ players }: { players: Player[] }) {
+  const onClickStartGame = async () => {
+    const { data, error } = await supabase
+      .from('sessions')
+      .update({ current_problem_sequence: 0 })
+      .eq('id', sessionId)
+    if (error) {
+      return alert(error.message)
+    }
+  }
+
+  return (
+    <div>
+      <div className="flex justify-start flex-wrap pb-4">
+        {players.map((player) => (
+          <div className="text-xl m-2 p-2 bg-green-500" key={player.id}>
+            {player.nickname}
+          </div>
+        ))}
+      </div>
+
+      <button
+        className="mx-auto bg-white py-4 px-12 block text-black"
+        onClick={onClickStartGame}
+      >
+        ゲームを始める
+      </button>
+    </div>
+  )
+}
+
+function Results() {
+  return <div></div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import React, { FormEvent, useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { setRequestMeta } from 'next/dist/server/request-meta'
 
-const supabase = createClient(
+export const supabase = createClient(
   'https://aofiufmhphqtsjpatqdy.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvZml1Zm1ocGhxdHNqcGF0cWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyNjY4MjIsImV4cCI6MjAxNzg0MjgyMn0.AE6Aq1h7mxmiWI0q-qc0NEcw42MNz0fJDWPWbckaiz0'
 )
 
-type Player = {
+export type Player = {
   nickname: string
   id: string
 }
 
-type Problem = {
+export type Problem = {
   id: string
   body: string
   order: number
@@ -22,36 +22,34 @@ type Problem = {
   choices: Choice[]
 }
 
-type Choice = {
+export type Choice = {
   id: string
   problem_id: string
   is_correct: boolean
   body: string
 }
 
-type Answer = {
+export type Answer = {
   id: string
   problem_id: string
   player_id: string
   choice_id: string
 }
 
-type Session = {
+export type Session = {
   id: string
   current_problem_sequence: number
   is_done: boolean
 }
 
-enum Screens {
+export enum Screens {
   register,
   lobby,
   quiz,
-  // problemShown,
-  // problemAnswering,
-  // problemAnswered,
-  // problemResult,
   results,
 }
+
+export const sessionId = 'dc84bced-bb8b-4bff-b7b1-9eb21cae92ca'
 
 export default function Home() {
   const onRegisterCompleted = (player: Player) => {
@@ -84,8 +82,6 @@ export default function Home() {
         rows.choices.filter((choice: Choice) => choice.is_correct).length
     )
   }
-
-  const sessionId = 'dc84bced-bb8b-4bff-b7b1-9eb21cae92ca'
 
   const [currentProblemSequence, setCurrentProblemSequence] = useState(0)
 
@@ -166,8 +162,14 @@ function Quiz({
 
   const [selectedChoiceId, setSelectedChoiceId] = useState<string>()
 
+  const [hasShownChoices, setHasShownChoices] = useState(false)
+
   useEffect(() => {
     setHasAnswered(false)
+
+    setTimeout(() => {
+      setHasShownChoices(true)
+    }, 5000)
   }, [problem.id])
 
   const answer = async (choiceId: string) => {
@@ -192,16 +194,14 @@ function Quiz({
       </div>
 
       <h1 className="pb-4 text-xl">{problem.body}</h1>
-      <div className="flex justify-between flex-wrap">
-        {problem.choices.map((choice) => (
-          <div key={choice.id} className="w-1/2 p-1">
-            <button
-              disabled={hasAnswered}
-              onClick={() => answer(choice.id)}
-              className={`p-2 w-full text-center 
-                ${
-                  selectedChoiceId == choice.id ? 'border-2 border-red-500' : ''
-                }
+      {hasShownChoices && (
+        <div className="flex justify-between flex-wrap">
+          {problem.choices.map((choice) => (
+            <div key={choice.id} className="w-1/2 p-1">
+              <button
+                disabled={hasAnswered}
+                onClick={() => answer(choice.id)}
+                className={`p-2 w-full text-center 
               ${
                 hasAnswered
                   ? selectedChoiceId == choice.id
@@ -213,12 +213,36 @@ function Quiz({
                     : 'bg-gray-400'
                   : 'bg-green-500'
               }`}
+              >
+                {choice.body}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+      {!hasShownChoices && (
+        <div className="text-center">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {choice.body}
-            </button>
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
@@ -273,6 +297,7 @@ function Register({
         type="text"
         onChange={(val) => setNickname(val.currentTarget.value)}
         placeholder="ニックネーム"
+        maxLength={20}
       />
       <button disabled={sending} className="w-full py-2 bg-green-500 mt-4">
         参加
