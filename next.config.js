7d390f03e9319ad0c88722dcb0c14427a2039c14
dshukertjr <dshukertjr@gmail.com>
2024-08-13 12:46:42 +0900

    feat: Add new questions and load question sets in descending order

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/host/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/host',
+        destination: '/host/dashboard',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
