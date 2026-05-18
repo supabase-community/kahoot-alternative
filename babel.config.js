4373f7db84b529811727ce8e8d3c9a19993fe155
test <test@test.com>
2026-05-18 18:13:40 +1000

    test: add src/app/host/page.test.tsx

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    ['@babel/preset-typescript', { 
+      isTSX: true,
+      allExtensions: true 
+    }],
+  ],
+};
